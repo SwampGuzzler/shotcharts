bc30a093bdfe32cc031176624ca244130f19ccfb
Lucas Cotner <lcotner@blueraster.com>
2015-08-12 17:51:12 -0400

    Inserting photo, cutting down charts, plotting frequency vs location

--- hexbin.py
+++ hexbin.py
@@ -30,21 +30,35 @@ pic = urllib.urlretrieve("http://stats.nba.com/media/players/230x185/101108.png"
 paul_pic = plt.imread(pic[0])
 
 img = OffsetImage(paul_pic, zoom=0.6)
+img.set_offset((300,275))
 cmap=plt.cm.YlOrRd_r
 
-plt.axis('off')
-plt.figure(figsize=(6,5.5))
-cp3 = plt.hexbin(shot_df.LOC_X, shot_df.LOC_Y, gridsize=[20,7])
+# plt.axis('off')
+# plt.figure(figsize=(12,11))
+# plt.figure(figsize=(6,5.5))
+# cp3 = plt.hexbin(shot_df.LOC_X, shot_df.LOC_Y, gridsize=[20,7])
 
 joint_shot_chart = sns.jointplot(shot_df.LOC_X, shot_df.LOC_Y, stat_func=None,
                                  kind='hex', space=0, gridsize=[20,7])
 
+ax = joint_shot_chart.ax_joint
 
-draw_court(outer_lines=True)
+ax.set_xlim(-250,250)
+ax.set_ylim(422.5, -47.5)
+
+# Get rid of axis labels and tick marks
+ax.set_xlabel('')
+ax.set_ylabel('')
+draw_court(ax)
+ax.set_title('James Harden FGA \n2014-15 Reg. Season', 
+             y=1.2, fontsize=18)
+ax.add_artist(img)
+
+# draw_court(outer_lines=True)
 plt.axis('off')
 plt.xlim(-300,300)
 plt.ylim(-100,500)
 # plt.imshow(paul_pic)
-# ax.add_artist(img)
+
 plt.show()
 
